44182785bc1a795dd777acf300856882b19ca25c
Yi <zghbhswty@gmail.com>
2023-11-28 11:38:59 +0800

    Update migrator.go
    支持全文索引

--- migrator.go
+++ migrator.go
@@ -54,12 +54,21 @@ func (m Migrator) CreateIndex(value interface{}, name string) error {
 
 			createIndexSQL := "CREATE "
 			if idx.Class != "" {
+				if idx.Class == "FULLTEXT"{
+					createIndexSQL += "CONTEXT" + " "
+					}
+				else{
 				createIndexSQL += idx.Class + " "
+					}
 			}
 			createIndexSQL += "INDEX ? ON ??"
 
 			if idx.Type != "" {
+				if idx.Class == "FULLTEXT"{
+					}
+				else{
 				createIndexSQL += " USING " + idx.Type
+					}
 			}
 
 			//if idx.Comment != "" {
